71b7ab91778984977c1b740a3a6df715d1cd9a28
Lucc <lukasz_rutkowski@live.com>
2026-01-29 13:29:10 +0100

    Add login and get user functionality to the backend app

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -42,6 +42,45 @@ export async function registerUser(req, res) {
     }
 }
 
-export async function loginUser(req, res) {}
+export async function loginUser(req, res) {
+    const { email, password } = req.body || {};
 
-export async function getUserInfo(req, res) {}
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ message: 'Both email and password are required' });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error logging user in',
+            error: error.message,
+        });
+    }
+}
+
+export async function getUserInfo(req, res) {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error getting user information',
+            error: error.message,
+        });
+    }
+}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,16 @@
 import express from 'express';
+
 import {
     registerUser,
     loginUser,
     getUserInfo,
 } from '../controllers/authController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/user', getUserInfo);
+router.get('/user', protect, getUserInfo);
 
 export default router;
